217546f70e26db2a05fa5344bcabd0160fc27cb0
Alex <alex_sizow@outlook.com>
2023-04-20 15:04:11 +0300

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template><div></div></template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  column-gap: 10px;
+}
+.buttons {
+  display: flex;
+  flex-direction: column;
+  flex-flow: column-reverse;
+}
+</style>
---
+++ src/components/LiftShaft.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template><div>f</div></template>
+
+<style lang="scss" scoped>
+.shaft {
+  width: 150px;
+
+  border: 2px solid rgb(137, 126, 126);
+  position: relative;
+}
+</style>
